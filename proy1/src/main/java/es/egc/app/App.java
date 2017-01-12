a32b89ca1fde03528f0298da3c020adcf8597223
copex91 <cope_1091@hotmail.com>
2017-01-12 21:06:43 +0100

    Creacion del proyecto
    Se ha creado el proyecto con una dependencia del repositorio de maven
    (selenium) y una dependencia local (jackson)

---
+++ proy1/src/main/java/es/egc/app/App.java
@@ -0,0 +1,15 @@
+package es.egc.app;
+import com.fasterxml.jackson.core.*;
+import org.openqa.selenium.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
